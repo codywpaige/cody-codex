d707f3c32afcd958e59315448ddb368f5e2fee28
codywpaige <109378374+codywpaige@users.noreply.github.com>
2022-10-08 10:16:03 -0400

    git update commit
    git update commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import * as _ from 'lodash' ;
 import { HashRouter, Switch } from 'react-router-dom'
 
 let App = () => {
-  let randomElement = _.sample([<RenderedResponse />, <CompletionsResponse />]);
+  // with lodash make a variable called RandomElement that when called, will choose one of the two [<RenderedResponse />, <CompletionsResponse />], and 99% that the same thing will not be selected
+  let randomElement = _.sample([<RenderedResponse />, <CompletionsResponse />], 2, false);
   const rootElement = document.getElementById("root");
   const root = createRoot(rootElement);
   root.render(
