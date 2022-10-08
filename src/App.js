292d7572b31aedee6695a4edf2adfccf54f2b65e
codywpaige <109378374+codywpaige@users.noreply.github.com>
2022-10-08 09:49:25 -0400

    updated routing
    updated routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import {Routes ,Route} from 'react-router-dom';
-// import the component renderedResponse.js
-import RenderedResponse from './components/renderedResponse';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import CompletionsResponse from './components/completionsResponse';
-import * as _ from 'lodash' ;
-import { HashRouter, Switch } from 'react-router-dom'
+import RenderedResponse from "./components/renderedResponse";
+import CompletionsResponse from "./components/completionsResponse";
+import * as _ from "lodash";
 
 let App = () => {
-  let randomElement = _.sample([<RenderedResponse />, <CompletionsResponse />]);
   const rootElement = document.getElementById("root");
   const root = createRoot(rootElement);
+  let randomElement = _.sample([<RenderedResponse />, <CompletionsResponse />]);
   root.render(
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
-        <Route exact path="/explain-like-five" component={CompletionsResponse} />
         <Route path="/explain-like-five" element={<CompletionsResponse/>}></Route>
         <Route path="/content-writer" element={<RenderedResponse/>}></Route>
         <Route path="/" element={randomElement}></Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
