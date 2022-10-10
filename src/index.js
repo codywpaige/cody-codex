28de59cf89301d4bd90da71f2651b15f5e38533d
codywpaige <109378374+codywpaige@users.noreply.github.com>
2022-10-09 21:53:37 -0400

    commit change
    commit change

--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ButtonAppBar from './components/ButtonAppBar';
-
+import { yellow } from '@mui/material/colors';
+import { color } from '@mui/system';
+import * as _ from 'lodash';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let randomColor = _.sample(['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#4B0082', '#8B00FF'], 1, false);
+document.body.style.backgroundColor = randomColor;
 root.render(
   <React.StrictMode>
     <App />
